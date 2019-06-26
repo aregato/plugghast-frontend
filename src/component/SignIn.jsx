5993477281f83c1bd57589242c166fe436142f20
aregato <hannes.linner@gmail.com>
2019-06-27 01:17:00 +0200

    registrering + login v1
    design

--- src/component/SignIn.jsx
+++ src/component/SignIn.jsx
@@ -28,48 +28,43 @@ class SignIn extends Component {
 
   render(){
     return(
-      <div className="sign-in-body">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-3 col-md-3 col-sm-0"></div>
-            <div className="col-lg-6 col-md-6 col-sm-12">
-              <div className="form well log-nav">
-                  <div className="form-nav">
-                    <h3 className="nav-reg"><Link to={'/signup'}>Registrera dig</Link></h3>
-                    <h3 className="nav-log nav-active"><Link to={'/signin'}>Logga in</Link></h3>
-                    <hr/>
-                  </div>
-                  <div className="log-inputs">
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="E-postadress"
-                    onChange={event => this.setState({email: event.target.value})}
-                  />
-                  <br/>
-
-                  <input
-                    className="form-control"
-                    type="password"
-                    placeholder="Lösenord"
-                    onChange={event => this.setState({password: event.target.value})}
-                  />
-                  </div>
-                  <a className="forget-psw">Glömt lösenord?</a>
-                  <br/>
-                  <div className="button-center">
-                    <button
-                      className="btn"
-                      type="button"
-                      onClick={() => this.signIn()}
-                    >
-                    Logga in
-                    </button>
-                  </div>
-              </div>
+      <div className="body">
+        <div className="row">
+          <div className="col-lg-4 col-md-3 col-sm-1"></div>
+          <div className="col-lg-3 col-md-6 col-sm-10">
+            <div className="well sign">
+                <div className="navigation-sign">
+                  <Link className="sign-up" to={'/signup'}>Registrera dig</Link>
+                  <Link className="sign-in dark" to={'/signin'}>Logga in</Link>
+                  <br />
+                  <br />
+                </div>
+                <div className="form-sign">
+                <input
+                  type="text"
+                  placeholder="E-postadress"
+                  onChange={event => this.setState({email: event.target.value})}
+                />
+                <br/>
+                <input
+                  type="password"
+                  placeholder="Lösenord"
+                  onChange={event => this.setState({password: event.target.value})}
+                />
+                </div>
+                <a className="forget-pws">Glömt lösenord?</a>
+                <br/>
+                <div className="button-sign">
+                  <button
+                    type="button"
+                    onClick={() => this.signIn()}
+                  >
+                  Logga in
+                  </button>
+                </div>
             </div>
-            <div className="col-lg-3 col-md-3 col-sm-0"></div>
           </div>
+          <div className="col-lg-4 col-md-3 col-sm-1"></div>
         </div>
       </div>
     )
--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -28,55 +28,51 @@ class SignOut extends Component {
 
   render(){
     return(
-      <div className="sign-in-body">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-3 col-md-3 col-sm-0"></div>
-            <div className="col-lg-6 col-md-6 col-sm-12">
-              <div className="form well log-nav">
-                  <div className="form-nav">
-                    <h3 className="nav-reg nav-active"><Link to={'/signup'}>Registrera dig</Link></h3>
-                    <h3 className="nav-log"><Link to={'/signin'}>Logga in</Link></h3>
-                    <hr/>
-                  </div>
-                  <div className="log-inputs">
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Namn"
-                  />
-                  <br />
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="E-postadress"
-                    onChange={event => this.setState({email: event.target.value})}
-                  />
-                  <br/>
-
-                  <input
-                    className="form-control"
-                    type="password"
-                    placeholder="Lösenord"
-                    onChange={event => this.setState({password: event.target.value})}
-                  />
-                  </div>
-                  
-                  <br/>
-                  <div>{this.state.error.message}</div>
-                  <div className="button-center">
-                    <button
-                      className="btn"
-                      type="button"
-                      onClick={() => this.signUp()}
-                    >
-                    Registrera dig
-                    </button>
-                  </div>
+      <div className="body">
+        <div className="row">
+          <div className="col-lg-4 col-md-3 col-sm-1"></div>
+          <div className="col-lg-3 col-md-6 col-sm-10">
+            <div className="well sign">
+              <div className="navigation-sign">
+                <Link className="sign-up dark" to={'/signup'}>Registrera dig</Link>
+                <Link className="sign-in" to={'/signin'}>Logga in</Link>
+                <br />
+                <br />
               </div>
+              <div className="form-sign">
+                <input
+                  type="text"
+                  placeholder="Namn"
+                />
+                <br />
+                <input
+                  type="text"
+                  placeholder="E-postadress"
+                  onChange={event => this.setState({email: event.target.value})}
+                />
+                <br/>
+
+                <input
+                  type="password"
+                  placeholder="Lösenord"
+                  onChange={event => this.setState({password: event.target.value})}
+                />
+                </div>
+
+                <br/>
+                <div>{this.state.error.message}</div>
+                <div className="button-sign">
+                  <button
+                    className=""
+                    type="button"
+                    onClick={() => this.signUp()}
+                  >
+                  Registrera dig
+                  </button>
+                </div>
             </div>
-            <div className="col-lg-3 col-md-3 col-sm-0"></div>
           </div>
+          <div className="col-lg-4 col-md-3 col-sm-1"></div>
         </div>
       </div>
     )
